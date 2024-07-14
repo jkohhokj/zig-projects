2b1fa613a92196e8c7b316cdf2b9d22d93b04091
Jayden <jkohhokj@gmail.com>
2024-07-14 19:02:16 -0400

    finished rsa suite

--- rsa_suite.zig
+++ rsa_suite.zig
@@ -4,49 +4,44 @@ const rng = std.crypto.random;
 
 pub fn main() !void {
     print("Generating primes:\n", .{});
-    var prime1 = generatePrimes();
-    var prime2 = generatePrimes();
+    const prime1 = generatePrime();
+    const prime2 = generatePrime();
     print("Primes p and q: {} {}\nProving they are prime:\n", .{ prime1, prime2 });
-    _ = try egcd(prime1, prime2, true);
+    _ = egcd(prime1, prime2, true);
 
-    var pubkey = try generatePubKey(prime1, prime2);
+    const pubkey = generatePubKey(prime1, prime2);
     print("\n\nFound the public key exponent\ne, n = {} \t {}\n", .{ pubkey.e, pubkey.n });
     print("Finding the private key exponent through inverse mod with egcd\n", .{});
-    var prikey = try generatePriKey(pubkey);
+    const prikey = generatePriKey(pubkey);
     print("\n\nFound the full keypair: \ne = {} \nd = {} \nn = {}\n", .{ prikey.e, prikey.d, prikey.n });
     print("Proving d * e === 1 (mod totient)\n", .{});
-    var quotient = @divFloor(@as(u256, prikey.d) * prikey.e, prikey.totient);
-    var remainder = @mod(@as(u256, prikey.d) * prikey.e, prikey.totient);
+    const quotient = @divFloor(@as(u256, prikey.d) * prikey.e, prikey.totient);
+    const remainder = @mod(@as(u256, prikey.d) * prikey.e, prikey.totient);
     print("{} * {} = {} * {} + {}\n", .{ prikey.d, prikey.e, prikey.totient, quotient, remainder });
 }
-pub fn generatePrimes() u128 {
-    const randint = rng.intRangeAtMost(u64, 3, 7);
+pub fn generatePrime() u128 {
+    const randint = rng.intRangeAtMost(u64, 20, 40);
     const euler_polynomial = randint * randint + randint + 41; //generate euler prime
-    const mersenne_prime = std.math.pow(u128, 2, euler_polynomial) - 1; //generate mersenne prime from euler prime
-    return mersenne_prime;
+    return euler_polynomial;
+    // const mersenne_prime = std.math.pow(u128, 2, euler_polynomial) - 1; //generate mersenne prime from euler prime
+    // return mersenne_prime;
 }
 
-pub fn generateSmallPrime() u128 {
-    const randint = rng.intRangeAtMost(u64, 1, 5);
-    const euler_polynomial = randint * randint + randint + 41; //generate euler prime
-    const mersenne_prime = std.math.pow(u128, 2, euler_polynomial) - 1; //generate mersenne prime from euler prime
-    return mersenne_prime;
-}
 const keypair = struct {
     e: u256 = undefined,
     d: u256 = undefined,
     n: u256 = undefined,
     totient: u256 = undefined,
 };
-pub fn generatePubKey(p: u128, q: u128) !keypair {
+pub fn generatePubKey(p: u128, q: u128) keypair {
     const totient: u256 = @as(u256, p - 1) * (q - 1);
-    var e: u256 = generateSmallPrime();
+    const e: u256 = generatePrime();
     const n = @as(u256, p) * q;
     return keypair{ .e = e, .n = n, .totient = totient };
 }
-pub fn generatePriKey(kp: keypair) !keypair {
-    var potential_kp = try egcd(kp.totient, kp.e, true);
-    var raw_d = @mod(@as(i257, potential_kp.t), @as(i257, kp.totient));
+pub fn generatePriKey(kp: keypair) keypair {
+    const potential_kp = egcd(kp.totient, kp.e, true);
+    const raw_d = @mod(@as(i257, potential_kp.t), @as(i257, kp.totient));
     return keypair{ .e = kp.e, .d = @as(u256, @intCast(raw_d)), .n = kp.n, .totient = kp.totient };
 }
 const row = struct {
@@ -63,12 +58,12 @@ const egcdResult = struct {
     s: i256,
     t: i256,
 };
-pub fn egcd(_a: u256, _b: u256, debug: bool) !egcdResult {
+pub fn egcd(_a: u256, _b: u256, debug: bool) egcdResult {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     var alloc = gpa.allocator();
-    var rows: u8 = 100;
+    const rows: u8 = 100;
     //const cols: u8 = 10;
-    var table = try alloc.alloc(row, rows);
+    var table = alloc.alloc(row, rows) catch unreachable;
     var a = @as(u256, _a);
     var b = @as(u256, _b);
     if (b > a) { //swap variables to make a > b
@@ -85,7 +80,7 @@ pub fn egcd(_a: u256, _b: u256, debug: bool) !egcdResult {
     table[c].s = 1; //s3
     table[c - 2].t = 0; // t1
     table[c - 1].t = 1; // t2
-    print("   a   |   b   |   q   |   r   |   s   |   t\n", .{});
+    print("  a  |  b  |  q  |  r  |  s  |  t\n", .{});
     while (table[c].r != 0) {
         table[c].q = @divTrunc(table[c].a, table[c].b); //q = a/b
         table[c].r = @mod(table[c].a, table[c].b); //r = a%b
